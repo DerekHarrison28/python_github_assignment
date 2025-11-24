6edd89c7eed690a48f1cdc3ca366ca4be9c2a35b
Derek Harrison <dharrison28@usf.edu>
2025-11-23 20:09:22 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+#Welcome Message to the program
 print("Welcome to my Python program!")
+
+#Gets users hours and turns it into a float then multiplies by 7 for weekly hours 
 hours = input("How many hours did you study today? ")
 hours = float(hours)
 weekly_hours = hours * 7
+
+#Prints calulated result
 print(f"You are on track to study {weekly_hours} hours this week.")
+
+#Error Prevention
 try:
     hours = float(hours)
 except ValueError:
